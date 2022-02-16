0e4fe66df8a3d8c5bd343998915cf98441612b9a
Djibril <djibrilsamassa.pro@gmail.com>
2022-02-16 14:26:58 +0100

    Route commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Favorites from "./Pages/Favorites";
+import Popular from "./Pages/Popular";
+import PopularBattle from "./Pages/PopularBattle";
+import Weekly from "./Pages/Weekly";
+import WeeklyBattle from "./Pages/WeeklyBattle";
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+
+    return <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/weekly" component={Weekly}/>
+      <Route exact path="/weekly-battle" component={WeeklyBattle}/>
+      <Route exact path="/popular" component={Popular}/>
+      <Route exact path="/popular-battle" component={PopularBattle}/>
+      <Route exact path="/favorites" component={Favorites}/>
+    </Switch>
+    </BrowserRouter>
+
+  };
+
 }
 
 export default App;
