d5195dff39d8dee1a668d7ebe6c279d0b764c68a
Djibril <djibrilsamassa.pro@gmail.com>
2022-02-16 15:47:53 +0100

    Movies commit

--- src/Pages/Popular.js
+++ src/Pages/Popular.js
@@ -12,7 +12,11 @@ class Popular extends React.Component{
     componentDidMount(){
         fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=94d06e89e3d1384687305d0385d6fcaa")
         .then((res) => res.json())
-        
+        .then((res) =>{
+            res.results.map((film) =>{
+                this.state.movies.push(film.title)
+            })
+        })
     };
 
 
