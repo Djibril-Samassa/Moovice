d02b84627cb19846d208b05ef409cf48d7aeda2e
Djibril <djibrilsamassa.pro@gmail.com>
2022-02-17 11:29:16 +0100

    movies commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Poster from "./poster.png";
 import "../App.css";
+import "../style/Card.css"
 
 class Card extends React.Component{
+
     render(){
         return(
             <div>
                 <img src={Poster} className="photo" alt="poster" />
-                <h2>Title: {this.props.title}</h2>
-                <p>Année de sortie: {this.props.year}</p>
-                <p>Description: {this.props.desc}</p>
+                <h2 className="title">Titre: {this.props.title}</h2>
+                <p className="year">Date de sortie: {this.props.year}</p>
+                <p className="desc">Description: {this.props.desc}</p>
             </div>
         )
     }
--- src/Pages/Popular.js
+++ src/Pages/Popular.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import Card from "../Components/Card"
 
 class Popular extends React.Component{
@@ -14,17 +15,26 @@ class Popular extends React.Component{
         fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=94d06e89e3d1384687305d0385d6fcaa")
         .then((res) => res.json())
         .then((res) =>{
-            res.results.map((film) =>{
-                this.state.movies.push(film)
-            })
+            this.setState({movies: (res).results})
         })
     };
+
 
 
     render(){
         return (
             <div>
                 <h1>Popular</h1>
+                <Link to="/">Homepage</Link>
+                {console.log(this.state.movies)}
+                {this.state.movies.map((movie) =>{
+                    return <Card
+                    poster={movie.poster_path}
+                    title={movie.original_title}
+                    year={movie.release_date}
+                    desc={movie.overview}
+                    />
+                })}
             </div>
         
         )
